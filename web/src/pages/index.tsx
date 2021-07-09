e6b206fb908d4b616ee9ed0eb14747470e6418d0
AlexArchibeque <wonkman33@gmail.com>
2021-07-09 16:09:16 -0700

    navbar implemented as well as verification for logged in/out

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { NavBar } from "../components/NavBar";
+
 const Index = () => (
-  <div>hello world for the last time nope jk. okay maybe? this is the one.</div>
+  <>
+    <NavBar />
+    <div>
+      hello world for the last time nope jk. okay maybe? this is the one.
+    </div>
+  </>
 );
 
 export default Index;
