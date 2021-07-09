fc8bc4450189f42ca270c5e93aff6427e3f2348f
AlexArchibeque <wonkman33@gmail.com>
2021-07-09 12:44:53 -0700

    there we go?

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,5 @@
-const Index = () => <div>hello world for the last time nope jk.</div>;
+const Index = () => (
+  <div>hello world for the last time nope jk. okay maybe?</div>
+);
 
 export default Index;
