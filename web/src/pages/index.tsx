51a8d2987b63c0e8b87de518c87803f3df7ddf18
AlexArchibeque <wonkman33@gmail.com>
2021-07-09 12:47:50 -0700

    last one for sure

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,5 @@
 const Index = () => (
-  <div>hello world for the last time nope jk. okay maybe?</div>
+  <div>hello world for the last time nope jk. okay maybe? this is the one.</div>
 );
 
 export default Index;
