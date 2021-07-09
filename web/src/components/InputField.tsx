cffb29ff3d44ee7136498acba282af0c94a3677f
AlexArchibeque <wonkman33@gmail.com>
2021-07-09 12:04:22 -0700

    new and fresh start for bestProject

---
+++ web/src/components/InputField.tsx
@@ -0,0 +1,24 @@
+import React, { InputHTMLAttributes } from "react";
+import { FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/input";
+import { useField } from "formik";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
