41b5c81a3fccae7fb8ad24b8f6cb17520ba56ed7
AlexArchibeque <wonkman33@gmail.com>
2021-07-15 20:57:46 -0700

    full typeorm transfer

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -15,7 +15,6 @@ export class PostResolver {
 
   @Mutation(() => Post)
   async createPost(@Arg("title") title: string): Promise<Post> {
-    // 2 sequel queries
     return Post.create({ title }).save();
   }
 
